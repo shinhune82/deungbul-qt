b0f7fefaf3be7c4e43efb87ae7fd65c3cc7adc6b
shinhune82 <shinhun.e82@gmail.com>
2026-06-19 09:43:12 +0900

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,16 +7,17 @@ import { getMessaging, isSupported as isMessagingSupported } from 'firebase/mess
 // 이 값들은 비밀키가 아니라 클라이언트 식별자라 코드에 그대로 둬도 괜찮습니다.
 // 실제 보안은 Firestore 보안 규칙(firestore.rules)이 담당합니다.
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID'
+  apiKey: "AIzaSyACbnHcH9OPbZurDjWf8m1cazFOSSadYKE",
+  authDomain: "deungbul-qt.firebaseapp.com",
+  projectId: "deungbul-qt",
+  storageBucket: "deungbul-qt.firebasestorage.app",
+  messagingSenderId: "1048157032708",
+  appId: "1:1048157032708:web:46aab740a361a9f5ebc051",
+  measurementId: "G-GQXXJHHKL7"
 }
 
 // Cloud Messaging > 웹 푸시 인증서에서 발급되는 VAPID 공개키
-export const VAPID_KEY = 'YOUR_VAPID_PUBLIC_KEY'
+export const VAPID_KEY = 'BCm1sLB19B--eH-e3FgGQhaxwZlWeoF7oA7Fa2V3FWZWE44quYgHDyugQNux5hIe7U7qhRSvRvLEERpojWaXqEo'
 
 export const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
